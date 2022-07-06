d380b0edbb3fb7ad71ad4212faa9272903dedf1c
nuts-rice <msis44443@gmail.com>
2022-07-06 15:40:46 -0400

    feat: lowest common ancestor

--- src/chp_3/invert_binary_search_tree.rs
+++ src/chp_3/invert_binary_search_tree.rs
@@ -19,24 +19,58 @@ impl TreeNode {
     }
 }
 
-impl Soln {
-    #[inline]
-    //Rc: single threaded refcounter
-    pub fn invert_tree(mut root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        Self::invert_tree_aux(&mut root);
-        root
+//Rc: single threaded refcounter
+pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    invert_tree_aux(&root);
+    root
+}
+
+pub fn invert_tree_aux(root: &Option<Rc<RefCell<TreeNode>>>) {
+    if let Some(root) = root {
+        let mut root = root.borrow_mut();
+
+        invert_tree_aux(&root.left);
+        invert_tree_aux(&root.right);
+
+        let left_node = std::mem::replace(&mut root.left, None);
+        let right_node = std::mem::replace(&mut root.right, left_node);
+        std::mem::replace(&mut root.left, right_node);
     }
+}
 
-    pub fn invert_tree_aux(root: Option<Rc<RefCell<TreeNode>>>) {
-        if let Some(root) = root {
-            let mut root = root.borrow_mut();
-
-            Self::invert_tree_aux(&mut root.left);
-            Self::invert_tree_aux(&mut root.right);
-
-            let left_node = std::mem::replace(&mut root.left, None);
-            let right_node = std::mem::replace(&mut root.right, left_node);
-            std::mem::replace(&mut root.left, right_node);
+pub fn lca_recursive(
+    root: &Option<Rc<RefCell<TreeNode>>>,
+    p: i32,
+    q: i32,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    if let Some(v) = root {
+        if let (Some(ref left), Some(ref right)) = (&v.borrow().left, &v.borrow().right) {
+            let node_value = v.borrow().val;
+            if p > node_value && q > node_value {
+                let right = Some(Rc::clone(right));
+                return lca_recursive(&right, p, q);
+            } else if p < node_value && q < node_value {
+                let left = Some(Rc::clone(left));
+                return lca_recursive(&left, p, q);
+            }
         }
+        return Some(Rc::new(RefCell::new(TreeNode::new(v.borrow().val))));
+    } else {
+        return None;
     }
+}
+
+pub fn lowest_common_ancestor(
+    root: Option<Rc<RefCell<TreeNode>>>,
+    p: Option<Rc<RefCell<TreeNode>>>,
+    q: Option<Rc<RefCell<TreeNode>>>,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    if p.is_none() || q.is_none() || root.is_none() {
+        return root;
+    }
+
+    let p_val = p.unwrap().borrow_mut().val;
+    let q_val = q.unwrap().borrow_mut().val;
+
+    return lca_recursive(&root, p_val, q_val);
 }
