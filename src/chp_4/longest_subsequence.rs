61cb6d4ea02d3b4009cc0b06f11c874c3ece2645
nuts-rice <msis44443@gmail.com>
2022-07-07 22:44:08 -0400

    feat: longest increasing subsequence

--- src/chp_4/longest_subsequence.rs
+++ src/chp_4/longest_subsequence.rs
@@ -39,14 +39,62 @@ pub fn longest_common_subsequence(a: &str, b: &str) -> String {
     result.iter().collect()
 }
 
+pub fn longest_increasing_subsequence<T: Ord + Clone>(input_array: &[T]) -> Vec<T> {
+    let n = input_array.len();
+    if n <= 1 {
+        return input_array.to_vec();
+    }
+
+    let mut increasing_sequence: Vec<(T, usize)> = Vec::new();
+    let mut previous = vec![0_usize; n];
+
+    increasing_sequence.push((input_array[0].clone(), 1));
+    for i in 1..n {
+        let value = input_array[i].clone();
+        if value > increasing_sequence.last().unwrap().0 {
+            previous[i] = increasing_sequence.last().unwrap().1 - 1;
+            increasing_sequence.push((value, i + 1));
+            continue;
+        }
+
+        let change_position = increasing_sequence
+            .binary_search(&(value.clone(), 0))
+            .unwrap_or_else(|x| x);
+        increasing_sequence[change_position] = (value, i + 1);
+        previous[i] = match change_position {
+            0 => i,
+            other => increasing_sequence[other - 1].1 - 1,
+        };
+    }
+
+    let mut out: Vec<T> = Vec::with_capacity(increasing_sequence.len());
+
+    out.push(increasing_sequence.last().unwrap().0.clone());
+    let mut current_index = increasing_sequence.last().unwrap().1 - 1;
+    while previous[current_index] != current_index {
+        current_index = previous[current_index];
+        out.push(input_array[current_index].clone());
+    }
+
+    out.into_iter().rev().collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::longest_common_subsequence;
-
+    use super::longest_increasing_subsequence;
     #[test]
-    fn test_longest_common_subsequence() {
+    fn longest_common_subsequence_test() {
         assert_eq!(&longest_common_subsequence("", ""), "");
         assert_eq!(&longest_common_subsequence("", "abcd"), "");
         assert_eq!(&longest_common_subsequence("abcd", "c"), "c");
+    }
+
+    #[test]
+    fn longest_increasing_subsequence_test_1() {
+        assert_eq!(
+            longest_increasing_subsequence(&vec![10, 9, 2, 5, 3, 7, 101, 18]),
+            vec![2, 3, 7, 18]
+        );
     }
 }
