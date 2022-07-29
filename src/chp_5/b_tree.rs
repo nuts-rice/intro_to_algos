156969901b11960afb1e9ac157e8b21fe051ece1
nuts-rice <msis44443@gmail.com>
2022-07-29 19:17:50 -0400

    feat: starting btree

---
+++ src/chp_5/b_tree.rs
@@ -0,0 +1,51 @@
+use std::convert::TryFrom;
+use std::fmt::Debug;
+use std::mem;
+
+struct Node<T> {
+    keys: Vec<T>,
+    children: Vec<Node<T>>,
+}
+
+pub struct BTree<T> {
+    root: Node<T>,
+    props: BTreeProps,
+}
+
+struct BTreeProps {
+    degree: usize,
+    max_keys: usize,
+    mid_key_idx: usize,
+}
+
+impl<T> Node<T>
+where
+    T: Ord,
+{
+    fn new(degree: usize, _keys: Option<Vec<T>>, _children: Option<Vec<Node<T>>>) -> Self {
+        Node {
+            keys: match _keys {
+                Some(_keys) => _keys,
+                None => Vec::with_capacity(degree - 1),
+            },
+            children: match _children {
+                Some(_children) => _children,
+                None => Vec::with_capacity(degree),
+            },
+        }
+    }
+
+    fn is_leaf(&self) -> bool {
+        self.children.is_empty()
+    }
+}
+
+impl BTreeProps {
+    fn new(degree: usize) -> Self {
+        BTreeProps {
+            degree,
+            max_keys: degree - 1,
+            mid_key_index: (degree - 1) / 2,
+        }
+    }
+}
