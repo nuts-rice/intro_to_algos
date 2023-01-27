a1e708288a7061ea940d390f39e8b4c5cb5b21f0
nuts-rice <msis44443@gmail.com>
2023-01-27 11:44:57 -0500

    🚧 wip

--- fun_stuff/src/type_session.rs
+++ fun_stuff/src/type_session.rs
@@ -1,5 +1,16 @@
+use yahoo_finance_api as yahoo;
+
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 use session_types::*;
+
+//makes a json request with this
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+    #[arg(short, long)]
+    index: String,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct StockInfo {
@@ -16,8 +27,27 @@ type CashBalance = Send<u64, Var<Z>>;
 
 type Client = <stockServer as HasDual>::Dual;
 
-fn stockServer(_c: Chan<(), stockServer>) {
-    unimplemented!()
+fn stockServer(c: Chan<(), stockServer>) {
+    let mut c = c.sel1().enter();
+    let mut balance = 0;
+    loop {
+        c = offer! {
+            c,
+            CashDeposit => {
+                todo!()
+            },
+            ViewStock => {
+                let args = Args::parse();
+                let (c, stock) = c.recv();
+                //do some json parsing of reply from finance api and send it back as stock info???
+                //c.send(StockInfo);
+                todo!()
+            },
+            CashBalance => {
+                todo!()
+            }
+        }
+    }
 }
 
 fn cash_deposit(c: Chan<(), Client>) {
@@ -25,7 +55,9 @@ fn cash_deposit(c: Chan<(), Client>) {
 }
 
 fn view_stock(c: Chan<(), Client>) {
-    unimplemented!()
+    let idx = Args::parse();
+    todo!()
+    //    let c = match c.send()
 }
 
 fn cash_balance(c: Chan<(), Client>) {
